d38c75adf5194927b362c9123e5beed450264241
Gildo Ernesto Paruque <76938257+GildoParuque@users.noreply.github.com>
2023-11-13 16:33:16 +0200

    Initial Commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production : true,
+  firebaseConfig:{
+    apiKey: "AIzaSyCjWYtpWlJXo0DfsTzegCovvw7nnQiD4j4",
+    authDomain: "ang-blog-15332.firebaseapp.com",
+    projectId: "ang-blog-15332",
+    storageBucket: "ang-blog-15332.appspot.com",
+    messagingSenderId: "767844383554",
+    appId: "1:767844383554:web:1b14a860a436cc103d66db"
+  }
+};
