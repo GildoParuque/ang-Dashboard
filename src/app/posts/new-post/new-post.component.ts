782a7b936d6b5c5d98d02a3c9c34f53fb1c56352
Gildo Ernesto Paruque <76938257+GildoParuque@users.noreply.github.com>
2023-11-16 16:53:23 +0200

    Muitas mundacas aplicadas

--- src/app/posts/new-post/new-post.component.ts
+++ src/app/posts/new-post/new-post.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoriesService } from 'src/app/services/categories.service';
 
 @Component({
   selector: 'app-new-post',
   templateUrl: './new-post.component.html',
   styleUrls: ['./new-post.component.css']
 })
-export class NewPostComponent {
+export class NewPostComponent implements OnInit {
+
+  permalink: string = '';
+  imgSrc: any = './assets/placeholder-image.png';
+  selectedImg: any;
+
+  categories: any;
+
+  constructor(private cotegoryService: CategoriesService){}
+
+  ngOnInit(): void {
+    this.cotegoryService.loadData().subscribe(val =>{
+        this.categories = val;
+    })
+  }
+
+  OnTitleChanged($event){
+
+    const title = $event.target.value;
+    this.permalink = title.replace(/\s/g, '-');
+
+  }
+
+  showPreview($event){
+    const reader = new FileReader();
+
+    reader.onload = (e) =>{
+      this.imgSrc = e.target.result
+    }
+
+    reader.readAsDataURL($event.target.files[0])
+    this.selectedImg = $event.target.files[0];
+
+  }
 
 }
